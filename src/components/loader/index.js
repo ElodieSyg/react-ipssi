62158c7e5db410c2eaca44560032327e7740919c
ElodieSayavong <elodie.sayavong.dev@gmail.com>
2022-06-30 09:58:10 +0200

    change loader form

--- src/components/loader/index.js
+++ src/components/loader/index.js
@@ -1,9 +1,18 @@
 import ContentLoader from "react-content-loader";
 
-const Loader = () => {
-    return (
-        <ContentLoader viewBox="0 0 380 70" />
-    );
+const Loader = props => {
+    <ContentLoader
+        viewBox="0 0 400 160"
+        height={160}
+        width={400}
+        backgroundColor="transparent"
+        {...props}
+    >
+        <circle cx="150" cy="86" r="8" />
+        <circle cx="194" cy="86" r="8" />
+        <circle cx="238" cy="86" r="8" />
+    </ContentLoader>
+
 };
 
 export default Loader;
